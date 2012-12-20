169f05c0182448fdabe0aa736d1909a8d64b1cdb
Emily Green <em@soundcloud.com>
2012-12-20 11:51:54 +0100

    add an in memory implementation that isnt based on acid state

--- src/Main.hs
+++ src/Main.hs
@@ -3,13 +3,14 @@ module Main (main) where
 import Control.Concurrent.Async
 import Data.Functor
 
-import AcidServer
+--import AcidServer
 import DataSource
+import InMemoryServer
 import WebServer
 
 
 main :: IO ()
-main = withAcidServer True 100 (\t -> t - 86400) $ \acid -> do
-  _ <- concurrently (DataSource.listen acid (lines <$> getContents) parseTriplet)
-                    (WebServer.start 8000 acid)
+main = withInMemoryServer 100 (\t -> t - 86400) $ \s -> do
+  _ <- concurrently (DataSource.listen s (lines <$> getContents) parseTriplet)
+                    (WebServer.start 8000 s)
   return ()
